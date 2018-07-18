a67536a070f41fac20e2daedcdbd43a582489c59
jagas <guzas.mindaugas@gmail.com>
2018-07-18 12:51:02 +0300

    test script 24

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   echo "firebase deploy --token 1/97vArtl4w8jJrcaH-fA2yU2OVyGcu1egvxN_rFP4zpo"
 else
-  echo 'firebase deploy -P development --token  "${FIREBASE_TOKEN}"'
+  echo 'firebase deploy -P development --token  "1/97vArtl4w8jJrcaH-fA2yU2OVyGcu1egvxN_rFP4zpo"'
 fi
